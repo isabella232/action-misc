d2e7f737dc9f2da3b1c1d29293230efa63821509
839 <8398a7@gmail.com>
2019-08-19 20:32:04 +0900

    add gh client

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
 
 async function run() {
   try {
     const myInput = core.getInput('myInput');
     core.debug(`Hello ${myInput}`);
+    gh();
   } catch (error) {
     core.setFailed(error.message);
   }
+}
+
+async function gh() {
+  const client = new github.GitHub((window as any).process.env.GITHUB_TOKEN);
+  const commit = await client.repos.getCommit();
+  core.debug(commit.data.commit.message);
 }
 
 run();
